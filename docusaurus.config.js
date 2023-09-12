af08e922f7e3a69738cbdcddfefac2e8939f0e23
琳汐露 <2048905214@qq.com>
2023-09-12 08:32:24 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Rinshiru', // Usually your GitHub org/user name.
-  projectName: 'RinshiruDocs', // Usually your repo name.
+  projectName: 'web_docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
